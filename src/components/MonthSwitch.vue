3c27d37873684cf99e3b00d1d84b4ecead0d5b65
Anna Golmakova <ann.golmakova@gmail.com>
2025-11-06 23:00:31 +0000

    Create month switcher

---
+++ src/components/MonthSwitch.vue
@@ -0,0 +1,61 @@
+<script setup lang="ts">
+import {
+  CalendarDate,
+  DateFormatter,
+  getLocalTimeZone,
+} from "@internationalized/date";
+import { ChevronLeft, ChevronRight } from "lucide-vue-next";
+import { computed } from "vue";
+
+interface Props {
+  date: CalendarDate;
+  locale?: string;
+}
+
+const { date, locale = "ru-RU" } = defineProps<Props>();
+
+const formatter = computed(() => new DateFormatter(locale, { month: "long" }));
+
+const formatTitle = (date: CalendarDate) => {
+  let month = formatter.value.format(date.toDate(getLocalTimeZone()));
+  month = month.charAt(0).toUpperCase() + month.slice(1);
+  return month + " " + date.year;
+};
+</script>
+
+<template>
+  <div class="switcher">
+    <button class="button" aria-label="Предыдущий месяц">
+      <ChevronLeft />
+    </button>
+    <div>
+      {{ formatTitle(date) }}
+    </div>
+    <button class="button" aria-label="Следующий месяц">
+      <ChevronRight />
+    </button>
+  </div>
+</template>
+
+<style scoped>
+.switcher {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+}
+.button {
+  display: grid;
+  place-items: center;
+  border: none;
+  border-radius: 8px;
+  background-color: transparent;
+  padding: 0;
+  width: 32px;
+  height: 32px;
+
+  &:hover {
+    background-color: oklch(0.94 0.015 250);
+  }
+}
+</style>
