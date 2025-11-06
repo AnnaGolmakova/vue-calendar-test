58bec336fd458a25d6e0639097ea062265d74be7
Anna Golmakova <ann.golmakova@gmail.com>
2025-11-06 15:41:07 +0000

    Create basic Day component

---
+++ src/components/Day.vue
@@ -0,0 +1,49 @@
+<script setup lang="ts">
+interface Props {
+  isSelected?: boolean;
+  isToday?: boolean;
+  label: string;
+}
+
+const { isSelected, isToday, label } = defineProps<Props>();
+</script>
+
+<template>
+  <button class="day" :class="{ selected: isSelected, today: isToday }">
+    {{ label }}
+  </button>
+</template>
+
+<style scoped>
+.day {
+  display: grid;
+  flex-direction: row;
+  place-items: center;
+  box-sizing: border-box;
+  border: none;
+  border-radius: 8px;
+  background-color: transparent;
+  aspect-ratio: 1;
+  min-width: 32px;
+  min-height: 32px;
+  font-size: 14px;
+  text-align: center;
+
+  &:hover {
+    background-color: oklch(0.94 0.015 250);
+  }
+}
+
+.selected {
+  background-color: oklch(0.9 0.02 250);
+  font-weight: 600;
+
+  &:hover {
+    background-color: oklch(0.9 0.02 250);
+  }
+}
+
+.today {
+  border: solid 1px oklch(0.9 0.02 250 / 40%);
+}
+</style>
